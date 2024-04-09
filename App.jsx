fb43529604a1e6282353e641bdf0c402b8a5ceae
bharathkumar89 <166509238+bharathkumar89@users.noreply.github.com>
2024-04-09 17:11:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+import PlanetsDirectory from './assets/PlanetsDirectory';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <header className="App-header text-center">
+        <h1>Star Wars Planets Directory</h1>
+      </header>
+      <main>
+        <PlanetsDirectory />
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
